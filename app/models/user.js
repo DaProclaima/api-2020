a12c3e873c3a69b74656d4fd586802d942464c4f
DaProclaima <sn.itpro@gmail.com>
2020-02-11 15:45:02 +0100

    feat: uses mongoose, mongodb, creates user model

---
+++ app/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema({
+  country: String,
+  city: String,
+  gender: String,
+  first_name: String,
+  last_name: String,
+  email: String,
+  password: String,
+  age: Number,
+  height: Number,
+  weight: Number,
+  city_code: String,
+  street_number: Number,
+  street_type: String,
+  street_name: String,
+  phone: String,
+  image_profil: {
+    type: String,
+    default: 'https://placehold.it/64x64'
+  }
+}, {
+  collection: 'users',
+  minimize: false,
+  versionKey: false
+}).set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+  }
+})
+
+module.exports = Schema
